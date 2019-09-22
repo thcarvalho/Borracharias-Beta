4d6747e553b723a22f8685c091d7ea9c898c8947
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2019-09-22 19:32:56 -0300

    destinações visualizadas como markers

--- src/components/maps.js
+++ src/components/maps.js
@@ -9,18 +9,38 @@ import {
   Dimensions,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import MapView from 'react-native-maps';
+import MapView,{ Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
-navigator.geolocation = require('@react-native-community/geolocation');
+import { recuperarDestinacoes } from "../Firebase";
 
 export default class Maps extends Component {
   state = {
     isLoading: true,
     region: null,
+    markers: [],
   };
+  
+  async mostrarDestinacoes(doc){
+    let id = doc.id;
+    let latitude = doc.data().latitude;
+    let longitude = doc.data().longitude;
+    let title = doc.data().ecoponto;
+    let description = doc.data().endereco + ', ' + doc.data().numero + ' - ' + doc.data().telefone;
+    
+    this.setState({ markers: this.state.markers.concat([{
+      id,
+      coordinate: {
+        latitude,
+        longitude,
+      },
+      title,
+      description,
+    }]) });
+  }
 
   async componentDidMount() {
+  recuperarDestinacoes(this);
   Geolocation.getCurrentPosition(
       position => {
         this.setState({
@@ -49,10 +69,18 @@ export default class Maps extends Component {
           style={styles.map}
           initialRegion={this.state.region}
           region={this.state.region}
-          loadingEnabled={true}
-          showsUserLocation={true}
-          followUserLocation={true}
-        />
+          loadingEnabled
+          showsUserLocation
+          followUserLocation
+        >
+          {this.state.markers.map(marker => (
+            <Marker 
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+            />
+          ))}
+        </MapView>
         <TouchableOpacity
           style={styles.toggle}
           onPress={this.props.navigation.openDrawer}>
