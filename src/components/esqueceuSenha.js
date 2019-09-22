4598689844f16413116ff1d7fd3cf65b528f459e
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2019-09-22 19:34:25 -0300

    pequenas mudanças

--- src/components/esqueceuSenha.js
+++ src/components/esqueceuSenha.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import {
+import { 
   Text,
   View,
   StyleSheet,
@@ -9,21 +9,25 @@ import {
   Alert
 } from 'react-native';
 
+const botaoPressionado = () => {
+	Alert.alert('Email enviado');
+};
+
 export default class EsqueceuSenha extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.titulo}>Esqueceu a senha?</Text>
-
+        
         <Text style={styles.texto}>Informe seu e-mail para que possa redefinir a senha</Text>
-
-        <TextInput style={styles.caixasTexto} underlineColorAndroid='transparent' placeholder='Email' />
-
-        <View>
-          <TouchableOpacity onPress={() => { alert('Email enviado'); }} style={styles.botao}>
-            <Text>Enviar</Text>
-          </TouchableOpacity>
-        </View>
+        
+        <TextInput style={styles.caixasTexto} underlineColorAndroid='transparent' placeholder='Email'/>
+        
+         <View>
+            <TouchableOpacity onPress={botaoPressionado} style={styles.botao}>
+		           <Text>Enviar</Text>
+	         </TouchableOpacity>
+            </View>
       </View>
     );
   }
@@ -31,36 +35,37 @@ export default class EsqueceuSenha extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#3CB371',
-    flex: 1,
-    alignItems: 'center',
-  },
+      backgroundColor: '#3CB371',
+      flex: 1,
+      alignItems: 'center',
+        },
   titulo: {
-    marginTop: 150,
-    marginBottom: 30,
-    color: '#fff',
-    fontSize: 38,
-    fontFamily: 'Arial',
-    fontWeight: 'bold',
+      marginTop: 150,
+      marginBottom: 30,
+      color: '#fff',
+      fontSize: 38,
+      fontFamily: 'Arial',
+      fontWeight: 'bold',
   },
   texto: {
     fontSize: 18,
     paddingHorizontal: 11,
   },
   caixasTexto: {
-    width: 338,
-    borderRadius: 10,
-    backgroundColor: '#fff',
-    padding: 14,
-    marginBottom: 10,
-    marginTop: 30,
+      width: 338,
+      borderRadius: 10,
+      backgroundColor: '#fff',
+      padding: 14,
+      marginBottom: 10,
+      marginTop: 30
   },
-  botao: {
-    alignItems: 'center',
-    width: 80,
-    borderRadius: 8,
-    backgroundColor: '#E0FFFF',
-    padding: 15,
-    marginTop: 25,
-  },
+    botao: {
+        alignItems: 'center',
+        width: 80,
+        borderRadius: 8,
+        backgroundColor: '#E0FFFF',
+        padding: 15,
+        marginTop: 25,
+    },
+    
 });
