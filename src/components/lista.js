8d373a8b510949f6233e7270e98f0333818fec51
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2019-09-30 01:30:29 -0300

    exibição de lista de ecopontos

--- src/components/lista.js
+++ src/components/lista.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { ScrollView } from 'react-native-gesture-handler';
 
+import { recuperarDestinacoes } from "../Firebase";
 
 export default class Lista extends Component {
- render() {
+state = {
+  destinacoes: [],
+};
+
+async mostrarDestinacoes(doc){
+  let id = doc.id;
+  let nome = doc.data().ecoponto;
+  let descricao = doc.data().endereco + ', ' + doc.data().numero;
+  
+  this.setState({ destinacoes: this.state.destinacoes.concat([{
+    id,
+    nome,
+    descricao,
+  }]) });
+}
+async componentDidMount(){
+  recuperarDestinacoes(this);
+}
+
+  render() {
     return (
-      <View style={{flex: 1}}>
+      <ScrollView style={{flex: 1}}>
         <TouchableOpacity style={{padding: 20}} onPress={this.props.navigation.openDrawer}>
           <Icon name="bars" size={30} color={'#ddd'}/>
         </TouchableOpacity>
         <Text style={{textAlign: 'center', textAlignVertical: 'center'}}>Lista</Text>
-
-      </View>
+        {this.state.destinacoes.map(destinacao => (
+            <View>
+              <Text>NOME ECOPONTO: {destinacao.nome}</Text>
+              <Text>ENDERECO: {destinacao.descricao}</Text>
+            </View>
+        ))}
+      </ScrollView>
     );
   }
 }
