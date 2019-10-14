dd026e307daaf6cdd6ee32cf62cc46a0ac47318e
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2019-10-14 13:43:36 -0300

    adicionado tipo de usuario

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,12 +1,10 @@
 /* eslint-disable prettier/prettier */
 class Usuario{
-    constructor(nome,email,userType){
+    constructor(nome,email){
         this.nome = nome;
         this.email = email;
-        this.userType = userType;
     }
     getNome = () => this.nome;
     getEmail = () => this.email;
-    getUserType = () => this.userType;
 }
 export default Usuario;
