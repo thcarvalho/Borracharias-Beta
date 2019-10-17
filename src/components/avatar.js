fcb2b6644b5431d7b337e33b2d3902e3c31ca000
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2019-10-17 11:01:06 -0300

    Coleta de usuario atual

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -5,7 +5,23 @@ import { View, ScrollView, StyleSheet, Text, Image } from 'react-native';
 import { SafeAreaView, DrawerItems } from "react-navigation";
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import Firebase from "../controller/Firebase";
+
 export default class Avatares extends Component {
+	state = {
+		user: '',
+	}
+	Firebase = new Firebase()
+	componentDidMount() {
+		this.Firebase.refUsuarios
+			.where('email', '==', this.Firebase.auth.currentUser.email)
+			.onSnapshot(snapshot => {
+				snapshot.forEach(doc => {
+					this.setState({ user: doc.data().nome });
+				})
+			})
+	}
+
 	render() {
 		return (
 			<SafeAreaView style={{ flex: 1 }} forceInset={{ top: "always", horizontal: "never" }}>
@@ -13,7 +29,7 @@ export default class Avatares extends Component {
 					<View style={{ marginTop: 10, alignItems: 'center', justifyContent: 'center' }}>
 						<Image style={styles.imagem} source={require('./assest/avatar-perfil.png')} />
 						<TouchableOpacity>
-							<Text style={styles.texto}>Teste</Text>
+							<Text style={styles.texto}>{this.state.user}</Text>
 						</TouchableOpacity>
 					</View>
 				</View>
